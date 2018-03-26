88edb78c302691b658213426cf752715d7a56b46
Joan <jsanchezgarcia13@gmail.com>
2018-03-26 09:19:13 +0200

    Log in screen changed to fragment

---
+++ app/src/main/java/me/integrate/socialbank/InsideActivity.java
@@ -0,0 +1,49 @@
+package me.integrate.socialbank;
+
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.support.v4.app.FragmentActivity;
+        import android.support.v4.app.FragmentManager;
+        import android.support.v4.app.FragmentTransaction;
+        import android.support.v7.app.ActionBarActivity;
+
+public class InsideActivity extends ActionBarActivity implements FragmentChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inside);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container_inside) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            LoginFragment firstFragment = new LoginFragment(); //it will be replaced by BoardFragment() when available
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container_inside, firstFragment).commit();
+        }
+    }
+
+    @Override
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container_inside, fragment, fragment.toString());
+        fragmentTransaction.addToBackStack(fragment.toString());
+        fragmentTransaction.commit();
+    }
+}
--- app/src/main/java/me/integrate/socialbank/LoginFragment.java
+++ app/src/main/java/me/integrate/socialbank/LoginFragment.java
@@ -3,9 +3,14 @@ package me.integrate.socialbank;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.Response;
@@ -15,12 +20,19 @@ import java.util.HashMap;
 
 public class LoginFragment extends Fragment {
     private static final String URL = "/login";
+    private EditText user;
+    private EditText password;
+    private Button LogIn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        user = (EditText)rootView.findViewById(R.id.email);
+        password = (EditText)rootView.findViewById(R.id.password);
+        LogIn = (Button)rootView.findViewById(R.id.log_in_button);
+        return rootView;
     }
 
     @Override
@@ -29,11 +41,61 @@ public class LoginFragment extends Fragment {
         getView().findViewById(R.id.log_in_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                postCredentials("user", "password");
-                //registerSelected();
-
+                if (user.getText().toString().length() != 0 && password.getText().toString().length() != 0) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Button Pressed!", Toast.LENGTH_LONG).show();
+                    postCredentials("user", "password");
+                }
+                //Log in treatment code here
             }
         });
+        getView().findViewById(R.id.register_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerSelected();
+            }
+        });
+        user.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                boolean b = user.getText().toString().length() != 0 && password.getText().toString().length() != 0;
+                if (b){enableButton(true);}
+                else {enableButton(false);}
+            }
+        });
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                boolean b = user.getText().toString().length() != 0 && password.getText().toString().length() != 0;
+                if (b){enableButton(true);}
+                else {enableButton(false);}
+            }
+        });
+    }
+
+    //Se extrae en función externa por si se quiere modificar el estilo
+    private void enableButton(Boolean enable) {
+        if (enable) LogIn.setEnabled(true);
+        else LogIn.setEnabled(false);
     }
 
     //función para llamar a la API
