aceac19dc1179e438fb4b7db2e4bdddd5cd86d93
Joan <jsanchezgarcia13@gmail.com>
2018-03-26 11:18:47 +0200

    Autocomplete register with Google Account

--- app/src/main/java/me/integrate/socialbank/RegisterFragment.java
+++ app/src/main/java/me/integrate/socialbank/RegisterFragment.java
@@ -1,20 +1,96 @@
 package me.integrate.socialbank;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class RegisterFragment extends Fragment {
+    private GoogleSignInClient mGoogleSignInClient;
+    private EditText name;
+    private EditText lastName;
+    private EditText email;
+    private Button SignUpGoogle;
+    private static final int RC_SIGN_IN = 9001;
 
+    Intent i;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_register, container, false);
+        name = (EditText) rootView.findViewById(R.id.editTextFirstName);
+        lastName = (EditText)rootView.findViewById(R.id.editTextLastName);
+        email = (EditText) rootView.findViewById(R.id.editTextEmail);
+        SignUpGoogle = (Button) rootView.findViewById(R.id.googleSignInButton);
+        initGoogleLogin();
+        return rootView;
 
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getView().findViewById(R.id.googleSignInButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+    }
 
+    private void initGoogleLogin() {
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity().getApplicationContext(), gso);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            if (account.getEmail().length() != 0) email.setText(account.getEmail());
+            if (account.getDisplayName().length() != 0) name.setText(account.getGivenName());
+            if (account.getFamilyName().length()!= 0) lastName.setText(account.getFamilyName());
+            //Todas las funciones que hacer cuando Google nos devuelve el mail
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Main Screen", "signInResult:failed code=" + e.getStatusCode());
+            //Excepción a lanzar
+        }
+    }
 }
