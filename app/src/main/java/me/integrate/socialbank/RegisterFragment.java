12a549aa57b4bd7b0137f6b8b30842cfeeaab35b
Joan <jsanchezgarcia13@gmail.com>
2018-03-26 10:49:03 +0200

    Register Fragment XML

--- app/src/main/java/me/integrate/socialbank/RegisterFragment.java
+++ app/src/main/java/me/integrate/socialbank/RegisterFragment.java
@@ -7,10 +7,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class RegisterFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false);
+
     }
+
+
 }
