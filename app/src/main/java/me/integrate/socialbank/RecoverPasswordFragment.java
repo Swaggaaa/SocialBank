f8f4debb8f5926be7b8262731d8c561f2bd9fa70
jordi987 <jordi987@gmail.com>
2018-04-04 19:06:38 +0200

    Update fragment_activity_recover_password.xml, strings.xml and RecoverPasswordFragment.java

--- app/src/main/java/me/integrate/socialbank/RecoverPasswordFragment.java
+++ app/src/main/java/me/integrate/socialbank/RecoverPasswordFragment.java
@@ -1,20 +1,61 @@
 package me.integrate.socialbank;
 
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.HashMap;
 
 public class RecoverPasswordFragment extends Fragment {
+
+    private EditText email;
+    private Button submit;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_activity_recover_password, container, false);
+        email = (EditText) rootView.findViewById(R.id.email_submit);
+        submit = (Button) rootView.findViewById(R.id.submit);
+        enableButton();
         return rootView;
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getView().findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HashMap<String, String> params = new HashMap<>();
+                params.put("email", email.getText().toString());
+           //     postCredentials(params);
+            }
+        });
+        email.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                enableButton();
+            }
+        });
+
+    }
+
+    private void enableButton() {
+        submit.setEnabled( email.getText().toString().length() != 0 );
+    }
+
 }
