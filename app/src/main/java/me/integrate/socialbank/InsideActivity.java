1d3a6f88cdca4b4728f85b4ad848ec33e7b1b551
Sergio Paredes <sergio.paredes@est.fib.upc.edu>
2018-04-21 23:57:55 +0200

    Change navigation drawer implementation. Extract toolbar layout

--- app/src/main/java/me/integrate/socialbank/InsideActivity.java
+++ app/src/main/java/me/integrate/socialbank/InsideActivity.java
@@ -1,66 +1,50 @@
 package me.integrate.socialbank;
 
-import android.content.res.Configuration;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-public class InsideActivity extends ActionBarActivity implements FragmentChangeListener {
+public class InsideActivity extends AppCompatActivity implements FragmentChangeListener {
     private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private String[] mPlanetTitles;
-    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inside);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar_inside);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        //TODO: arreglar que se vea completo el fragment
-        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        // set up the drawer's list view with items and click listener
-        mDrawerList.setAdapter(new ArrayAdapter<>(this,
-                R.layout.drawer_list_item, mPlanetTitles));
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    // set item as selected to persist highlight
+                    menuItem.setChecked(true);
+                    // close drawer when item is tapped
+                    mDrawerLayout.closeDrawers();
 
+                    // Add code here to update the UI based on the item selected
+                    // For example, swap UI fragments here
+                    if (menuItem.getTitle().equals(getResources().getString(R.string.logout)))
+                        logout();
 
-        // ActionBarDrawerToggle ties together the the proper interactions
-        // between the sliding drawer and the action bar app icon
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                mDrawerLayout,         /* DrawerLayout object */
-                myToolbar,  /* nav drawer image to replace 'Up' caret */
-                R.string.drawer_open,  /* "open drawer" description for accessibility */
-                R.string.drawer_close  /* "close drawer" description for accessibility */
-        ) {
-            public void onDrawerClosed(View view) {
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-        if (savedInstanceState == null) {
-            selectItem(0);
-        }
-        mDrawerLayout.addDrawerListener(mDrawerToggle);
+                    return true;
+                });
 
         // Check that the activity is using the layout version with
         // the fragment_container FrameLayout
@@ -87,49 +71,19 @@ public class InsideActivity extends ActionBarActivity implements FragmentChangeL
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // The action bar home/up action should open or close the drawer.
-        // ActionBarDrawerToggle will take care of this.
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle action buttons
         switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggle
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    //TODO: enviar el fragment correspondiente
-    private void selectItem(int position) {
-        switch (position) {
-            case 1:
-                Log.v("test", "1");
-                break;
-            case 2:
-                Log.v("test", "2");
-                break;
-            default:
-                Log.v("test", "3");
-                break;
-        }
-        // update selected item and title, then close the drawer
-        mDrawerList.setItemChecked(position, true);
-        setTitle(mPlanetTitles[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
+    private void logout() {
+        SharedPreferencesManager.INSTANCE.remove(this, "token");
+        startActivity(new Intent(this, MainActivity.class));
     }
 
     @Override
@@ -141,11 +95,4 @@ public class InsideActivity extends ActionBarActivity implements FragmentChangeL
         fragmentTransaction.commit();
     }
 
-    /* The click listener for ListView in the navigation drawer */
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
 }
