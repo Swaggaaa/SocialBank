58694f4a8ccc2f307f5e4299b95ba410bf217922
Ferran Ramírez <ferranramireznavajon@gmail.com>
2018-04-21 18:03:58 +0200

    CreateEvent: inflated menu, image functionality

--- app/src/main/java/me/integrate/socialbank/CreateEventFragment.java
+++ app/src/main/java/me/integrate/socialbank/CreateEventFragment.java
@@ -1,16 +1,24 @@
 package me.integrate.socialbank;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -23,12 +31,15 @@ import android.widget.Toast;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
-import java.io.File;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
+import static android.content.ContentValues.TAG;
 
 public class CreateEventFragment extends Fragment {
 
     private static final String URL = "/recover";
-    private Button buttonCreate;
     private ImageButton buttonImg;
     private Button buttonAsk;
     private Button buttonOffer;
@@ -36,12 +47,66 @@ public class CreateEventFragment extends Fragment {
     private Button buttonNoFixed;
     private Button buttonYesGroup;
     private Button buttonNoGroup;
+    private EditText name;
+    private EditText address;
+    private EditText description;
     private TableLayout groupTable;
     private TableRow capacityRow;
     private int eventType;
     private int eventFixed;
     private int eventGroup;
 
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
+    private static final int MY_PERMISSIONS_REQUEST_MANAGE_DOCUMENTS = 2;
+    private boolean canWeRead;
+    private boolean thereisPic;
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_create_event, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_createEvent) {
+            if( areFilled() )
+                //TODO: fer be
+                postCredentials("asd","gsdf");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void postCredentials(String newPassword, String token) {
+        //TODO: tot, canviar camps dentrada
+        APICommunicator apiCommunicator = new APICommunicator();
+        Response.Listener responseListener = new Response.Listener<CustomRequest.CustomResponse>() {
+            @Override
+            public void onResponse(CustomRequest.CustomResponse response) {
+                Toast.makeText(getActivity().getApplicationContext(), "holi", Toast.LENGTH_LONG).show();
+                eventsSelected();
+            }
+        };
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                String message;
+                int errorCode = error.networkResponse.statusCode;
+                if (errorCode >= 500  &&  errorCode <= 511)
+                    message = "Server error";
+                else if(errorCode == 400)
+                    message = "Bad request";
+                else if(errorCode == 404)
+                    message = "Wrong token";
+                else
+                    message = "Unexpected error";
+                Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        };
+
+        apiCommunicator.putRequest(getActivity().getApplicationContext(), URL.concat("/".concat(token)), responseListener, errorListener, newPassword);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -49,7 +114,6 @@ public class CreateEventFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_create_event, container, false);
 
-        buttonCreate = (Button) rootView.findViewById(R.id.buttonCreate);
         buttonImg = (ImageButton) rootView.findViewById(R.id.buttonImg);
         buttonAsk = (Button) rootView.findViewById(R.id.buttonAsk);
         buttonOffer = (Button) rootView.findViewById(R.id.buttonOffer);
@@ -57,8 +121,16 @@ public class CreateEventFragment extends Fragment {
         buttonNoFixed = (Button) rootView.findViewById(R.id.buttonNoFixed);
         buttonYesGroup = (Button) rootView.findViewById(R.id.buttonYesGroup);
         buttonNoGroup = (Button) rootView.findViewById(R.id.buttonNoGroup);
+
+        name = (EditText) rootView.findViewById(R.id.editTextName);
+        address = (EditText) rootView.findViewById(R.id.editTextAddress);
+        description = (EditText) rootView.findViewById(R.id.editTextDescription);
+
         groupTable = (TableLayout) rootView.findViewById(R.id.groupTable);
         capacityRow = (TableRow) rootView.findViewById(R.id.capacityRow);
+
+        thereisPic = false;
+        canWeRead = checkPermissions();
 
         eventType = -1;
         eventFixed = -1;
@@ -77,16 +149,10 @@ public class CreateEventFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        buttonCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                buttonCreate.setEnabled(false);
-            }
-        });
         view.findViewById(R.id.buttonImg).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity().getApplicationContext(), "IMAGE BUTTON!", Toast.LENGTH_LONG).show();
+                readGallery();
             }
         });
         view.findViewById(R.id.buttonAsk).setOnClickListener(new View.OnClickListener() {
@@ -148,4 +214,101 @@ public class CreateEventFragment extends Fragment {
         return false;
     }
 
+    private boolean areFilled() {
+        if( !thereisPic )
+            Toast.makeText(getActivity(), "You must set an image", Toast.LENGTH_SHORT).show();
+        else if( !name.getText().toString().isEmpty()
+                && !address.getText().toString().isEmpty()
+                && !description.getText().toString().isEmpty())
+            Toast.makeText(getActivity(), "Fill all fields", Toast.LENGTH_SHORT).show();
+        else if( eventFixed == -1 )
+            Toast.makeText(getActivity(), "Set schedule used", Toast.LENGTH_SHORT).show();
+        else if( eventType == -1 )
+            Toast.makeText(getActivity(), "Set event type", Toast.LENGTH_SHORT).show();
+        else return true;
+        return false;
+    }
+
+    private void readGallery() {
+        PermissionChecker.checkReadExternalStoragePermissions(getActivity(),MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        Intent pickAnImage = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickAnImage.setType("image/*");
+
+        startActivityForResult(pickAnImage, 2);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == 2 ) {
+                thereisPic = true;
+                data.getData();
+                Uri selectedImage = data.getData();
+                if(canWeRead && requestCode == 2){
+                    Log.v(TAG, "Selected image uri" + selectedImage);
+                }
+                Log.v(TAG, String.valueOf(selectedImage));
+                loadImageFromUri(selectedImage);
+            }
+        }
+        else{
+            Log.v("Result","Something happened when tried to get the image");
+        }
+    }
+
+    private void loadImageFromUri(Uri imageUri) {
+        try {
+            buttonImg.setImageBitmap(MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String convertUriToBase64(Uri imageUri) {
+        String imagePath = imageUri.toString();
+        Bitmap bm = BitmapFactory.decodeFile(imagePath);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] byteArrayImage = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private boolean checkPermissions() {
+        PermissionChecker.checkReadExternalStoragePermissions(getActivity(), MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        return ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_DENIED;
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    canWeRead = true; //Permission granted
+                else
+                    canWeRead = false; //Permission denied
+                return;
+            }
+            case  MY_PERMISSIONS_REQUEST_MANAGE_DOCUMENTS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    canWeRead = true; //Permission granted
+                else
+                    canWeRead = false; //Permission denied
+                return;
+            }
+        }
+    }
+
+    private void eventsSelected() {
+        //TODO: Choose the proper Fragment (not created yet
+        Fragment eventsFragment = new LoginFragment();
+        FragmentChangeListener fc = (FragmentChangeListener) getActivity();
+        fc.replaceFragment(eventsFragment);
+    }
 }
