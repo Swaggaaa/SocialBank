4397b418cf4fa6b9225cd34b66da93a8638ca733
Joan <jsanchezgarcia13@gmail.com>
2018-05-22 14:18:18 +0200

    MyAccount fragment

---
+++ app/src/main/java/me/integrate/socialbank/MyAccountFragment.java
@@ -0,0 +1,19 @@
+package me.integrate.socialbank;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyAccountFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_my_account, container, false);
+        return rootView;
+    }
+}
