82acea62993a87d1a2a0df81f6df2e028cdd46e4
Sergi Ibanyez Pena <sergi.ibanyez@est.fib.upc.edu>
2018-06-12 11:50:39 +0200

    Implemented basic purchase hours functionality - missing back end

--- app/src/main/java/me/integrate/socialbank/MyAccountFragment.java
+++ app/src/main/java/me/integrate/socialbank/MyAccountFragment.java
@@ -19,41 +19,67 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Response;
+import com.braintreepayments.api.BraintreeFragment;
+import com.braintreepayments.api.PayPal;
+import com.braintreepayments.api.exceptions.BraintreeError;
+import com.braintreepayments.api.exceptions.ErrorWithResponse;
+import com.braintreepayments.api.exceptions.InvalidArgumentException;
+import com.braintreepayments.api.interfaces.BraintreeCancelListener;
+import com.braintreepayments.api.interfaces.BraintreeErrorListener;
+import com.braintreepayments.api.interfaces.PaymentMethodNonceCreatedListener;
+import com.braintreepayments.api.models.PayPalRequest;
+import com.braintreepayments.api.models.PaymentMethodNonce;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class MyAccountFragment extends Fragment {
+public class MyAccountFragment extends Fragment implements PaymentMethodNonceCreatedListener,
+        BraintreeErrorListener, BraintreeCancelListener {
     private static final String URL = "/users";
     private TextView accountStatus;
     private TextView accountStatusImage;
     private TextView verifyAccountHint;
     private EditText sendRequestText;
     private Button sendRequestButton;
+    private Button purchaseHoursButton;
     private TextView accountBalanceHint;
     private TextView userBalance;
-    private Button buyHours;
     private boolean verified;
     private ProgressDialog loadingDialog;
     private String email;
+    private BraintreeFragment mBrainTreeFragment;
+    private List<HoursPackage> hoursPackages;
+    private HoursPackage chosenHoursPackage;
+
+
+    enum TransactionResults {
+        ACCEPTED, REJECTED
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_my_account, container, false);
+        hoursPackages.add(new HoursPackage("Basic Package", 99.9, 100));
+        hoursPackages.add(new HoursPackage("Professional Organization Package", 399.9, 500));
+        hoursPackages.add(new HoursPackage("Premium Organization Package", 1199.9, 2000));
         accountStatus = (TextView) rootView.findViewById(R.id.AccountStatus);
         accountStatusImage = (TextView) rootView.findViewById(R.id.account_verified);
         verifyAccountHint = (TextView) rootView.findViewById(R.id.account_verify_hint);
         sendRequestText = (EditText) rootView.findViewById(R.id.editText_request);
         sendRequestButton = (Button) rootView.findViewById(R.id.button_send_request);
+        purchaseHoursButton = (Button) rootView.findViewById(R.id.button_buy_hours);
         accountBalanceHint = (TextView) rootView.findViewById(R.id.account_balance_hint);
         userBalance = (TextView) rootView.findViewById(R.id.user_balance);
-        buyHours = (Button) rootView.findViewById(R.id.button_buy_hours);
-        email = SharedPreferencesManager.INSTANCE.read(getActivity(),"user_email");
+        email = SharedPreferencesManager.INSTANCE.read(getActivity(), "user_email");
         loadingDialog = ProgressDialog.show(getActivity(), "",
                 getString(R.string.loadingMessage), true);
         loadScreen(email);
@@ -79,9 +105,8 @@ public class MyAccountFragment extends Fragment {
                 if (verified) {
                     accountStatusImage.setVisibility(View.VISIBLE);
                     accountStatus.setText(R.string.verified);
-                    buyHours.setVisibility(View.VISIBLE);
-                }
-                else {
+                    purchaseHoursButton.setVisibility(View.VISIBLE);
+                } else {
                     accountStatus.setText(R.string.standard);
                     verifyAccountHint.setVisibility(View.VISIBLE);
                     sendRequestText.setVisibility(View.VISIBLE);
@@ -123,8 +148,8 @@ public class MyAccountFragment extends Fragment {
             params.put("message ", sendRequestText.getText().toString());
             sendRequest(params);
         });
-        buyHours.setOnClickListener(v -> {
-            //ready per comprar hores
+        purchaseHoursButton.setOnClickListener(v -> {
+            purchaseHours();
         });
     }
 
@@ -152,8 +177,80 @@ public class MyAccountFragment extends Fragment {
 
         };
 
-        apiCommunicator.postRequest(getActivity().getApplicationContext(), URL+'/'+email+"/verified", responseListener, errorListener, params);
+        apiCommunicator.postRequest(getActivity().getApplicationContext(), URL + '/' + email + "/verified", responseListener, errorListener, params);
     }
 
+    private void purchaseHours() {
+        try {
+            mBrainTreeFragment = BraintreeFragment.newInstance(this.getActivity(), "sandbox_zr8s9syj_y3bwvxzq3zwfhvw9");
+        } catch (InvalidArgumentException ex) {
+            ex.printStackTrace();
+        }
 
+        mBrainTreeFragment.addListener(this);
+
+        //TODO: JOAN; AQUI MARCA EL PACKAGE KLK
+        chosenHoursPackage = hoursPackages.get(0);
+
+        PayPalRequest paypalRequest = new PayPalRequest(String.valueOf(chosenHoursPackage.getPrice()))
+                .currencyCode("EUR")
+                .displayName(chosenHoursPackage.getName())
+                .intent(PayPalRequest.INTENT_AUTHORIZE);
+        PayPal.requestOneTimePayment(mBrainTreeFragment, paypalRequest);
+    }
+
+    @Override
+    public void onPaymentMethodNonceCreated(PaymentMethodNonce paymentMethodNonce) {
+        APICommunicator apiCommunicator = new APICommunicator();
+        Response.Listener responseListener = (Response.Listener<CustomRequest.CustomResponse>) response -> {
+            TransactionResults transactionResults = TransactionResults.valueOf(response.response);
+            if (transactionResults == TransactionResults.ACCEPTED) {
+                Toast.makeText(getActivity().getApplicationContext(), R.string.thank_you_purchase, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getActivity().getApplicationContext(), R.string.payment_rejected, Toast.LENGTH_LONG).show();
+            }
+        };
+
+        Response.ErrorListener errorListener = error -> {
+            String message;
+            int errorCode = error.networkResponse == null ? -1 : error.networkResponse.statusCode;
+            if (errorCode == 401)
+                message = "Unauthorized";
+            else if (errorCode == 403)
+                message = "Forbidden";
+            else if (errorCode == 404)
+                message = "Not Found";
+            else
+                message = "Unexpected error";
+
+            loadingDialog.dismiss();
+            Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        };
+
+        Map<String, String> params = new HashMap<>();
+        params.put("amount", String.valueOf(chosenHoursPackage.getPrice()));
+        params.put("nonce", paymentMethodNonce.getNonce());
+        apiCommunicator.postRequest(getActivity().getApplicationContext(), URL, responseListener, errorListener, params);
+    }
+
+    @Override
+    public void onCancel(int requestCode) {
+        Toast.makeText(getActivity().getApplicationContext(), R.string.payment_not_finished, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onError(Exception error) {
+        if (error instanceof ErrorWithResponse) {
+            ErrorWithResponse errorWithResponse = (ErrorWithResponse) error;
+            BraintreeError cardErrors = errorWithResponse.errorFor("creditCard");
+            if (cardErrors != null) {
+                // There is an issue with the credit card.
+                BraintreeError expirationMonthError = cardErrors.errorFor("expirationMonth");
+                if (expirationMonthError != null) {
+                    // There is an issue with the expiration month.
+                    System.out.println(expirationMonthError.getMessage());
+                }
+            }
+        }
+    }
 }
