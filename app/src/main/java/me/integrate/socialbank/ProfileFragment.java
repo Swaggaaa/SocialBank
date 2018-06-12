1aa60d4db1a0cfedbdb3223037e76f6de660424b
elenaalonso <32187906+elenaalonso@users.noreply.github.com>
2018-06-12 20:23:42 +0200

    Merged with develop

--- app/src/main/java/me/integrate/socialbank/ProfileFragment.java
+++ app/src/main/java/me/integrate/socialbank/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
