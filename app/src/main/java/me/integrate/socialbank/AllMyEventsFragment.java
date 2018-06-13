d13ddb5b08f7fcced5e8ddcd9feb3023f7fd0940
jordi987 <jordi987@gmail.com>
2018-06-13 10:46:49 +0200

    Merged with develop

--- app/src/main/java/me/integrate/socialbank/AllMyEventsFragment.java
+++ app/src/main/java/me/integrate/socialbank/AllMyEventsFragment.java
@@ -213,13 +213,13 @@ public class AllMyEventsFragment extends Fragment {
     private void errorTreatment(int errorCode) {
         String message;
         if (errorCode == 401)
-            message = getString(R.string.Unauthorized);
+            message = getString(R.string.unauthorized);
         else if(errorCode == 403)
-            message = getString(R.string.Forbidden);
+            message = getString(R.string.forbidden);
         else if(errorCode == 404)
-            message = getString(R.string.NotFound);
+            message = getString(R.string.not_found);
         else
-            message = getString(R.string.UnexpectedError);
+            message = getString(R.string.unexpectedError);
 
         loadingDialog.dismiss();
         Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_LONG).show();
--- app/src/main/java/me/integrate/socialbank/BoardFragment.java
+++ app/src/main/java/me/integrate/socialbank/BoardFragment.java
@@ -245,14 +245,14 @@ public class BoardFragment extends Fragment {
     private void errorTreatment(int errorCode) {
         String message;
         if (errorCode == 401)
-            message = getString(R.string.Unauthorized);
+            message = getString(R.string.unauthorized);
         else if(errorCode == 403)
-            message = getString(R.string.Forbidden);
+            message = getString(R.string.forbidden);
         else if(errorCode == 404)
-            message = getString(R.string.NotFound);
+            message = getString(R.string.not_found);
         else
-            message = getString(R.string.UnexpectedError);
+            message = getString(R.string.unexpectedError);
 
-        Toast.makeText(getActivity().getApplicationContext(), errorCode + " - " + message, Toast.LENGTH_LONG).show();
+        Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 }
